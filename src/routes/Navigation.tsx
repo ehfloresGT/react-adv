e5eac0e79b3cd2fcfd6b1f567cd723ff8304b99d
ehflores <ehflores@gmail.com>
2023-09-01 15:28:06 -0600

    Commit Component Compound

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
+
 import { showActive } from '../helpers/showActive';
 import logo from '../logo.svg'
 
@@ -10,7 +12,7 @@ export const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={ showActive }>Home</NavLink>
+              <NavLink to="/home" className={ showActive }>Shopping</NavLink>
             </li>
             <li>
               <NavLink to="/about" className={ showActive }>About</NavLink>
@@ -22,7 +24,7 @@ export const Navigation = () => {
         </nav>
 
         <Routes>
-          <Route path="home" element={<h1>Home Page</h1>} />
+          <Route path="home" element={<ShoppingPage />} />
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
           <Route path="/*" element={<Navigate to="/home" replace />} />
